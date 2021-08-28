d7165b26ce26c3233a962c8dc7299741874921d9
Oshada1988 <lahirupr471@gmail.com>
2021-08-28 08:33:57 +0530

    create mongodb connection

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors")
+const dotenv = require("dotenv");
+require("dotenv").config();
+
+
+/*---------------------------------------Set Up Server-------------------------------------------*/
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin:["http://localhost:3000"],
+    credentials:true
+}));
+
+const PORT = process.env.PORT||5000;
+app.listen(PORT,()=>{
+    console.log(`Server start on port : ${PORT}`)
+})
+
+
+
+/*---------------------------------------connect to mongoDB-------------------------------------------*/
+    //Type 01
+    const URL= process.env.MONGO_CONNECT;
+
+    mongoose.connect(URL,{
+        
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+       
+    })
+    const connection =mongoose.connection;
+    connection.once("open", ()=>{
+        console.log("connection success")
+    })
